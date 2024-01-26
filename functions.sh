14463761a1a9d3d7e2100b4aea8a598077247158
Mike Watkins <github@mikewatkins.ca>
2024-01-26 09:45:59 -0800

    check, don't be lazy

--- functions.sh
+++ functions.sh
@@ -169,7 +169,9 @@ EOF
 		echo "$FIXMAPS" | tee -a $MAPFILE
 		gzip $MAPFILE
 		# in case this was a chroot install
-		echo "KEYMAP=us" | tee -a /etc/rc.conf
+		if ! grep -e "^KEYMAP=us" /etc/rc.conf; then
+			echo "KEYMAP=us" | tee -a /etc/rc.conf
+		fi
 	fi
 
 	# install and allow reconfiguration of linux if needed
